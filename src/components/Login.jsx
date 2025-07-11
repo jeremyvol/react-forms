99bdf4e59671109bbd0d8e0f9d3e02b4a11fbaac
Jeremy Vollant <j.vollant@gmail.com>
2025-07-11 22:54:47 +0200

    RefLogin

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,30 +1,14 @@
-import { useState } from 'react';
+import { useRef } from 'react';
 
 export default function Login() {
-  // const [enteredEmail, setEnteredEmail] = useState('');
-  // const [enteredPassword, setEnteredPassword] = useState('');
-  const [enteredValues, setEnteredValues] = useState({
-    email: '',
-    password: '',
-  });
+  const email = useRef();
+  const password = useRef();
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(enteredValues);
-  }
 
-  // function handleEmailChange(event) {
-  //   setEnteredEmail(event.target.value);
-  // }
-  // function handlePasswordChange(event) {
-  //   setEnteredPassword(event.target.value);
-  // }
-
-  function handleInputChange(identifier, value) {
-    setEnteredValues(prevValues => ({
-      ...prevValues,
-      [identifier]: value,
-    }));
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
   }
 
   return (
@@ -34,26 +18,12 @@ export default function Login() {
       <div className='control-row'>
         <div className='control no-margin'>
           <label htmlFor='email'>Email</label>
-          <input
-            id='email'
-            type='email'
-            name='email'
-            onChange={event => handleInputChange('email', event.target.value)}
-            value={enteredValues.email}
-          />
+          <input id='email' type='email' name='email' ref={email} />
         </div>
 
         <div className='control no-margin'>
           <label htmlFor='password'>Password</label>
-          <input
-            id='password'
-            type='password'
-            name='password'
-            onChange={event =>
-              handleInputChange('password', event.target.value)
-            }
-            value={enteredValues.password}
-          />
+          <input id='password' type='password' name='password' ref={password} />
         </div>
       </div>
 
