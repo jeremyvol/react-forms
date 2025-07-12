82a061fcb4b3a0faec00e11f18f23a6a1738de93
Jeremy Vollant <j.vollant@gmail.com>
2025-07-12 10:34:27 +0200

    form validation

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -1,29 +1,34 @@
 import { useState } from 'react';
 
 export default function Login() {
-  // const [enteredEmail, setEnteredEmail] = useState('');
-  // const [enteredPassword, setEnteredPassword] = useState('');
   const [enteredValues, setEnteredValues] = useState({
     email: '',
     password: '',
   });
+
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
 
   function handleSubmit(event) {
     event.preventDefault();
     console.log(enteredValues);
   }
 
-  // function handleEmailChange(event) {
-  //   setEnteredEmail(event.target.value);
-  // }
-  // function handlePasswordChange(event) {
-  //   setEnteredPassword(event.target.value);
-  // }
-
   function handleInputChange(identifier, value) {
     setEnteredValues(prevValues => ({
       ...prevValues,
       [identifier]: value,
+    }));
+  }
+
+  function handleInputBlur(identifier) {
+    setDidEdit(prevEdit => ({
+      ...prevEdit,
+      [identifier]: true,
     }));
   }
 
@@ -38,9 +43,13 @@ export default function Login() {
             id='email'
             type='email'
             name='email'
+            onBlur={() => handleInputBlur('email')}
             onChange={event => handleInputChange('email', event.target.value)}
             value={enteredValues.email}
           />
+          <div className='control-error'>
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className='control no-margin'>
